b3821cfee17e004647d01d41ba1bcae9991e2e34
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-01 19:44:36 +0530

    deleted 3d earth data

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -5,8 +5,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
 //import 'Home.dart';
-import 'planet.dart';
-import 'planet_page.dart';
+import 'Home.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -18,6 +17,6 @@ class SplashScreen extends StatelessWidget {
         splashIconSize: 250,
         backgroundColor: Colors.black,
         pageTransitionType: PageTransitionType.rightToLeftWithFade,
-        nextScreen: PlanetPage());
+        nextScreen: HomePage());
   }
 }
