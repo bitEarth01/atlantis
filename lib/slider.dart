75ddc04502e6a7202a7bc6569633c888e96ffbbb
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-02 02:42:17 +0530

    added new page

---
+++ lib/slider.dart
@@ -0,0 +1,16 @@
+import 'Home.dart';
+import 'package:flutter/material.dart';
+
+class slider extends StatefulWidget {
+  const slider({super.key});
+
+  @override
+  State<slider> createState() => _sliderState();
+}
+
+class _sliderState extends State<slider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
