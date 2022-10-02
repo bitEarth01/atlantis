922a8fe912a648114f5229f5cc5706698a5565c7
Manoj <manojparasuram.sadhanala@gmail.com>
2022-10-02 08:51:10 +0530

    added background

--- lib/slider.dart
+++ lib/slider.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'Home.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_sliders/sliders.dart';
@@ -16,62 +18,10 @@ class _sliderState extends State<slider> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Scaffold(
-      backgroundColor: Color.fromARGB(255, 33, 88, 113),
-      body: SafeArea(
-        child: Stack(
-          alignment: Alignment.topLeft,
-          children: [
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 250),
-              child: SfSlider(
-                min: DateTime(2000, 01, 01, 00),
-                max: DateTime(2020, 12, 31, 24),
-                value: _value,
-                interval: 5,
-                activeColor: Colors.redAccent,
-                inactiveColor: Color.fromARGB(255, 237, 161, 133),
-                showTicks: true,
-                showLabels: false,
-                dateIntervalType: DateIntervalType.years,
-                enableTooltip: true,
-                minorTicksPerInterval: 1,
-                onChanged: (dynamic value) {
-                  setState(() {
-                    _value = value;
-                  });
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 500),
-              child: SfSlider(
-                min: 1,
-                max: 8,
-                value: _newvalue,
-                interval: 1,
-                activeColor: Colors.redAccent,
-                inactiveColor: Color.fromARGB(255, 237, 161, 133),
-                showTicks: true,
-                showLabels: true,
-                enableTooltip: true,
-                minorTicksPerInterval: 1,
-                onChanged: (dynamic value) {
-                  setState(() {
-                    _newvalue = value;
-                  });
-                },
-              ),
-            ),
-          ],
-        ),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/back1.jpg"), fit: BoxFit.cover),
       ),
-    ));
+    );
   }
 }
